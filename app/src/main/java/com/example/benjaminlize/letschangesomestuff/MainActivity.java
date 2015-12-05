807efdb05298a386b667db3553e41735e6b7bd26
ben <benji20002000@hotmail.com>
2015-12-05 22:46:48 +0000

    changed the value :O

--- app/src/main/java/com/example/benjaminlize/letschangesomestuff/MainActivity.java
+++ app/src/main/java/com/example/benjaminlize/letschangesomestuff/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int blabloblu = 1;
+        int blabloblu = 2000;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
